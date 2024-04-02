a3472f498bf70f9b31bba34e93bbd5c5cfba8739
Przemyslaw Poskrobko <przemyslaw.poskrobko@gmail.com>
2024-04-02 22:33:33 +0200

    Very first commit; configuration properties for MySQL database

---
+++ src/main/java/com/example/SimpleBookLibraryInSpring/SimpleBookLibraryInSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.SimpleBookLibraryInSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBookLibraryInSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBookLibraryInSpringApplication.class, args);
+	}
+
+}
